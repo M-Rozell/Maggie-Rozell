2d2c6b1b35eb1f0746479a442ab3e513c4cc849a
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-11-15 15:15:31 -0600

    Updates

---
+++ static/js/856.27425573.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaggie_rozell=self.webpackChunkmaggie_rozell||[]).push([[856],{6856:function(t,a,e){e.d(a,{oTc:function(){return n}});var l=e(9983);function n(t){return(0,l.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 5.63l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83a.996.996 0 000-1.41z"}}]})(t)}}}]);
+//# sourceMappingURL=856.27425573.chunk.js.map
