4e0692d1c58430392f13feca760bf9a8cb6dca04
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-11-17 11:12:19 -0600

    added loading background

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ const App = () => {
 
     const [isLoading, setLoading] = useState(true);
   
-  function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+  function loadingRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
     return new Promise(resolve => setTimeout(() => resolve(), 2500));
   } 
 
   useEffect(() => {
-    someRequest().then(() => {
+    loadingRequest().then(() => {
       const loaderElement = document.querySelector(".loader-container");
       if (loaderElement) {
         loaderElement.remove();
