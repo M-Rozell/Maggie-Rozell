f9959bd887bb3923011fc032ecdf2d7ad37061b7
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2023-01-02 17:45:59 -0600

    working on loading

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
+import React, {  lazy, Suspense } from "react";
 import MainHome from "./pages/MainHome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const RandMHome = lazy(() => import("./components/rAndMComponents/RandMHome"));
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
 
-  function loadingRequest() {
-    //Simulates a request; makes a "promise" that'll run for 2.5 seconds
-    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
-  }
-
-  useEffect(() => {
-    loadingRequest().then(() => {
-      const loaderElement = document.querySelector(".loader-container");
-      if (loaderElement) {
-        loaderElement.remove();
-        setLoading(!isLoading);
-      }
-    });
-  });
-
-  if (isLoading) {
-    return null;
-  }
 
   return (
     <BrowserRouter>
